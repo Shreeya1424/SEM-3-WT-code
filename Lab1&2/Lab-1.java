adf08c65a4384ee87563abc26579decbe29cc2d1
Shreeya1424 <140989885+Shreeya1424@users.noreply.github.com>
2024-06-22 16:56:13 +0530

    Add files via upload

---
+++ Lab1&2/Lab-1.java
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Lab-1</title>
+</head>
+<body>
+	<script type="text/javascript">
+		let a =parseInt(prompt("enter her!!"))
+		let b =parseInt(prompt("enter her!!"))
+		let c =prompt("enter for add,sub,mul,div")
+		if(c=="add"){
+			document.write(a+b)
+		}
+		else if(c=="sub"){
+			document.write(a-b)
+		}
+		else if(c=="mul"){
+			document.write(a*b)
+		}
+		else if(c=="div"){
+			document.write(a/b)
+		}
+</script>
+
+</body>
+</html>
